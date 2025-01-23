cdc5ab2be31cddaa797e78af02406938c03c55b2
GFXGFX1 <topbrawlerdemon@gmail.com>
2025-01-23 21:47:37 +0300

    Update igra.py
    Призраки

--- igra.py
+++ igra.py
@@ -278,6 +278,28 @@ def startGame():
                     self.rect.left = old_x
                     self.rect.top = old_y
 
+    class Ghost(Player):
+        def changespeed(self, list, ghost, turn, steps, l):
+            try:
+                z = list[turn][2]
+                if steps < z:
+                    self.change_x = list[turn][0]
+                    self.change_y = list[turn][1]
+                    steps += 1
+                else:
+                    if turn < l:
+                        turn += 1
+                    elif ghost == "clyde":
+                        turn = 2
+                    else:
+                        turn = 0
+                    self.change_x = list[turn][0]
+                    self.change_y = list[turn][1]
+                    steps = 0
+                return [turn, steps]
+            except IndexError:
+                return [0, 0]
+
     pygame.init()
 
     screen = pygame.display.set_mode([606, 606])
