4a5c3c15bccd52d67585a65ff2d5a95cc87c39e6
GFXGFX1 <topbrawlerdemon@gmail.com>
2025-02-03 19:16:37 +0300

    Update igra.py

--- igra.py
+++ igra.py
@@ -8,6 +8,7 @@ from PyQt6.QtCore import pyqtSignal, QObject
 import threading
 import csv
 import re
+from map import walls
 
 # Определяем цвета
 black = (0, 0, 0)
@@ -283,16 +284,6 @@ def startGame(score_signal, username):
 
     def setupRoomOne(all_sprites_list):
         wall_list = pygame.sprite.RenderPlain()
-        walls = [[0, 0, 6, 600], [0, 0, 600, 6], [0, 600, 606, 6], [600, 0, 6, 606],
-                 [300, 0, 6, 66], [60, 60, 186, 6], [360, 60, 186, 6], [60, 120, 66, 6],
-                 [60, 120, 6, 126], [180, 120, 246, 6], [300, 120, 6, 66], [480, 120, 66, 6],
-                 [540, 120, 6, 126], [120, 180, 126, 6], [120, 180, 6, 126], [360, 180, 126, 6],
-                 [480, 180, 6, 126], [180, 240, 6, 126], [180, 360, 246, 6], [420, 240, 6, 126],
-                 [240, 240, 42, 6], [324, 240, 42, 6], [240, 240, 6, 66], [240, 300, 126, 6],
-                 [360, 240, 6, 66], [0, 300, 66, 6], [540, 300, 66, 6], [60, 360, 66, 6],
-                 [60, 360, 6, 186], [480, 360, 66, 6], [540, 360, 6, 186], [120, 420, 366, 6],
-                 [120, 420, 6, 66], [480, 420, 6, 66], [180, 480, 246, 6], [300, 480, 6, 66],
-                 [120, 540, 126, 6], [360, 540, 126, 6]]
 
         for item in walls:
             wall = Wall(item[0], item[1], item[2], item[3], blue)
@@ -620,7 +611,9 @@ def startGame(score_signal, username):
             pygame.display.flip()
 
             clock.tick(60)
+
     startGame()
+
 
 def update_user_score(username, new_score):
     try:
